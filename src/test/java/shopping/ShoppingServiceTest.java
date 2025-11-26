d382f22dd236e1721d56ff49d49fcfee7337d168
Daria Glebova <glebova555dasha@gmail.com>
2025-11-27 03:33:56 +0500

    ref: исправила замечания

--- src/test/java/shopping/ShoppingServiceTest.java
+++ src/test/java/shopping/ShoppingServiceTest.java
@@ -2,6 +2,7 @@ package shopping;
 
 import customer.Customer;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -21,40 +22,66 @@ class ShoppingServiceTest {
 
     private final ProductDao productDaoMock;
     private final ShoppingService shoppingService;
+    private Customer customer1;
+
     ShoppingServiceTest(@Mock ProductDao productDaoMock) {
         this.productDaoMock = productDaoMock;
         shoppingService = new ShoppingServiceImpl(productDaoMock);
     }
 
     /**
+     * Инициализация тестового окружения:
+     * Создание экземляра покупателя
+     */
+    @BeforeEach
+    void initEach() {
+        this.customer1 = new Customer(1, "111");
+    }
+
+    /**
      * Проверка получения корзины покупателя
      * Проверяем на граничных данных
      * Проверяем, что продукты сохраняются в корзине
-     * Проверяем, что кол-во продуктов суммируется в корзине
      * Проверяем, что продукты у каждого покупателя свои
      */
     @Test
     @DisplayName("getCart - позитивная проверка")
     void getCartTest() {
-        Customer customer1 = new Customer(1, "111");
         Customer customer2 = new Customer(2, "222");
 
         Product product1 = new Product("test1", 5);
         Product product2 = new Product("test2", 5);
 
         shoppingService.getCart(customer1).add(product1, 1);
-        shoppingService.getCart(customer1).add(product1, 1);
         shoppingService.getCart(customer2).add(product2, 5);
 
         Cart cart1 = shoppingService.getCart(customer1);
         Cart cart2 = shoppingService.getCart(customer2);
 
+        Assertions.assertNotSame(cart1, cart2);
+
         Assertions.assertTrue(cart1.getProducts().containsKey(product1));
-        Assertions.assertEquals(2, cart1.getProducts().get(product1));
         Assertions.assertFalse(cart1.getProducts().containsKey(product2));
 
         Assertions.assertFalse(cart2.getProducts().containsKey(product1));
         Assertions.assertTrue(cart2.getProducts().containsKey(product2));
+    }
+
+    /**
+     * Проверка получения корзины покупателя
+     * Проверяем, что кол-во продуктов суммируется в корзине
+     */
+    @Test
+    @DisplayName("getCart - суммирование одного продукта после нескольких добавлений")
+    void getCartWithSumProductsTest() {
+        Product product1 = new Product("test1", 5);
+
+        shoppingService.getCart(customer1).add(product1, 1);
+        shoppingService.getCart(customer1).add(product1, 1);
+
+        Cart cart1 = shoppingService.getCart(customer1);
+
+        Assertions.assertEquals(2, cart1.getProducts().get(product1));
     }
 
     /**
@@ -63,7 +90,7 @@ class ShoppingServiceTest {
     @Test
     @DisplayName("getAllProducts - позитивная проверка")
     void getAllProductsTest() {
-        // В методе нет логики для проверки
+        // Внутри только вызов метода из dao
     }
 
     /**
@@ -72,12 +99,11 @@ class ShoppingServiceTest {
     @Test
     @DisplayName("getProductByName - позитивная проверка")
     void getProductByNameTest() {
-        // В методе нет логики для проверки
+        // Внутри только вызов метода из dao
     }
 
     /**
      * Проверка покупки продуктов
-     * Проверяем на граничных данных
      * Проверяем, что продуктов остаётся после покупки верное кол-во
      * Проверяем, что после покупки корзина пустая
      * Проверяем изменения в бд
@@ -85,26 +111,25 @@ class ShoppingServiceTest {
     @Test
     @DisplayName("buy - позитивная проверка")
     void buyTest() throws BuyException {
-        Customer customer1 = new Customer(1, "111");
-        Customer customer2 = new Customer(2, "222");
-
         Product product1 = new Product("test1", 5);
-        Product product2 = new Product("test2", 5);
-
         shoppingService.getCart(customer1).add(product1, 1);
-        shoppingService.getCart(customer2).add(product2, 5);
 
         Assertions.assertTrue(shoppingService.buy(shoppingService.getCart(customer1)));
-        Assertions.assertTrue(shoppingService.buy(shoppingService.getCart(customer2)));
-
         Assertions.assertEquals(4, product1.getCount());
-        Assertions.assertEquals(0, product2.getCount());
-
-        Assertions.assertEquals(0, shoppingService.getCart(customer1).getProducts().size());
-        Assertions.assertEquals(0, shoppingService.getCart(customer2).getProducts().size());
+        Assertions.assertTrue(shoppingService.getCart(customer1).getProducts().isEmpty());
 
         Mockito.verify(productDaoMock).save(product1);
-        Mockito.verify(productDaoMock).save(product2);
+    }
+
+    /**
+     * Проверка покупки всего кол-ва продуктов
+     */
+    @Test
+    @DisplayName("buy - покупка всех продуктов")
+    void buyAllProductsTest() {
+        Product product1 = new Product("test1", 5);
+        shoppingService.getCart(customer1).add(product1, 5);
+        Assertions.assertEquals(0, product1.getCount());
     }
 
     /**
@@ -116,24 +141,15 @@ class ShoppingServiceTest {
      */
     @Test
     @DisplayName("buy - недостаточно продуктов")
-    void buyWhenNotEnoughProductsTest() throws BuyException {
-        Customer customer1 = new Customer(1, "111");
-        Customer customer2 = new Customer(2, "222");
-
+    void buyWhenNotEnoughProductsTest() {
         Product product1 = new Product("test1", 5);
-
         Cart cart1 = shoppingService.getCart(customer1);
-        Cart cart2 = shoppingService.getCart(customer2);
 
         cart1.add(product1, 1);
-        cart2.add(product1, 5);
+        product1.subtractCount(5);
 
-        Assertions.assertTrue(shoppingService.buy(cart1));
-        BuyException e = Assertions.assertThrows(BuyException.class, () -> shoppingService.buy(cart2));
+        BuyException e = Assertions.assertThrows(BuyException.class, () -> shoppingService.buy(cart1));
         Assertions.assertEquals("В наличии нет необходимого количества товара 'test1'", e.getMessage());
-        Assertions.assertEquals(4, product1.getCount());
-
-        Mockito.verify(productDaoMock, Mockito.times(1)).save(product1);
     }
 
     /**
@@ -142,7 +158,17 @@ class ShoppingServiceTest {
     @Test
     @DisplayName("buy - пустая корзина")
     void buyEmptyCartTest() throws BuyException {
-        Customer customer1 = new Customer(1, "111");
         Assertions.assertFalse(shoppingService.buy(shoppingService.getCart(customer1)));
+        Mockito.verify(productDaoMock, Mockito.never()).save(Mockito.any());
+    }
+
+    /**
+     * Проверка покупки null
+     */
+    @Test
+    @DisplayName("buy - null")
+    void buyNullTest() throws BuyException {
+        Assertions.assertFalse(shoppingService.buy(null));
+        Mockito.verify(productDaoMock, Mockito.never()).save(Mockito.any());
     }
 }
